e126bf02b60153936d06f6fdc31f3bda58cc44a7
Brian Zhen <bzhen@berkeley.edu>
2018-03-09 22:05:14 -0800

    added option to choose ticker and 200-day avg

---
+++ README.md
@@ -0,0 +1,21 @@
+# Project Title
+
+Simple webpage for displaying stock data.
+
+## Getting Started
+
+After unzipping, from the `stock_chart/` directory, create a virtualenv and install the necessary packages.
+
+```
+$ virtualenv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
+```
+$ python stock_chart.py
+```
+
+## Built With
+
+* [Flask](http://flask.pocoo.org/) - The web framework used
+* [Plotly](https://plot.ly/javascript/) - Javascript Graphing Library
--- stock_chart.py
+++ stock_chart.py
@@ -1,17 +1,25 @@
-# from flask import Flask, flash, redirect, render_template, request, session, abort
 import quandl
 from flask import Flask, render_template
-app = Flask(__name__)
+from werkzeug.contrib.cache import SimpleCache
 
+app = Flask(__name__)
+cache = SimpleCache()
 quandl.ApiConfig.api_key = "zC992yeEkw5VTye5PFJY"
  
 @app.route("/")
 def index():
     return render_template('stock_chart.html')
 
-@app.route("/data")
-def aapl_data():
-    data = quandl.get("EOD/AAPL", returns="pandas", paginate=True)
+@app.route("/data/<string:ticker>/")
+def display_data(ticker):
+    def retrieve_data(ticker):
+        return quandl.get("EOD/{}".format(ticker), paginate=True)
+    
+    data = cache.get(ticker)
+    if data is None:
+        data = retrieve_data(ticker)
+        data['200Day_Avg'] = data['Close'].rolling(200).mean() # add moving average
+        cache.set(ticker, data)
     return data.to_csv()
  
 if __name__ == "__main__":
